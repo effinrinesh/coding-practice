75ef38218e3acdd8243605efbdb793238063e465
effinrinesh <effinrineshj@gmail.com>
2024-12-30 09:06:56 +0000

    few more excercises on Dictionaries and Lists (Py)

--- python-coding-practice/ex9-un.py
+++ python-coding-practice/ex9-un.py
@@ -5,16 +5,36 @@
 
 
 filename = input("Please enter the file name: ")
+
 try:
     fh = open(filename)
 except:
     print("Enter a valid file name")
+    quit()
+    
 mailid = dict()
+
 for line in fh:
     if line.startswith("From:"):
         pos = line.find(" ")
         atpos = line.find(" ", pos+1)
-        mailid = line[pos+1:atpos]
-        #quickly refresh histogram creation & .get method and come back
-    
-
+        key = line[pos+1:atpos]
+        mailid[key] = mailid.get(key,0) + 1
+        
+print(mailid)
+       # if key in mailid:
+        #    mailid[key] = mailid[key] + 1
+       # else:
+        #    mailid[key] = 1
+        #quickly refresh histogram creation concepts & .get method and come back
+        # okay key value pairs are kinda clear now
+        # learn histogram idioms
+#practicing a bit for max looping
+#print(mailid)
+#bignum = None
+#bigname = None
+#for kk,vv in mailid.items():
+#    if bignum is None or vv > bignum:
+#        bignum = vv
+#        bigname = kk
+#print(bigname,bignum)
--- python-coding-practice/ex9.4.py
+++ python-coding-practice/ex9.4.py
@@ -1,17 +1,48 @@
-#9.4 Write a program to read through the mbox-short.txt and figure out who has sent the greatest number of mail messages. The program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail. The program creates a Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file. After the dictionary is produced, the program reads through the dictionary using a maximum loop to find the most prolific committer.
-
-
-#fname = input('Enter the file name: ')
-#if not fname = "mbox-short.txt":
- #   quit()
-fh = open('mbox-short.txt')
-names = dict()
+#9.4 Write a program to read through the mbox-short.txt and figure 
+# out who has sent the greatest number of mail messages. The program
+#  looks for 'From ' lines and takes the second word of those lines as 
+# the person who sent the mail. The program creates a Python dictionary
+#  that maps the sender's mail address to a count of the number of times they
+#  appear in the file. After the dictionary is produced, the program reads
+#  through the dictionary using a maximum loop to find the most prolific committer.
+'''
+fh = open("mbox-short.txt")
+mailid = dict()
 for line in fh:
-    if 'From ' in line:
+    if line.startswith("From "):
         pos = line.find(' ')
-        spos = line.find(' ', pos + 1)
-        username = line[pos : spos - 1]
-        print(username)
-        
-print(names)
-        
+        endpos = line.find(' ', pos + 1)
+        key = line[pos+1:endpos]
+        mailid[key] = mailid.get(key,0) + 1
+#max looping through the dictionary
+bigname = None
+bigval = None
+for mailname,mailcount in mailid.items():
+    if bigval is None or mailcount > bigval:
+        bigval = mailcount
+        bigname = mailname
+
+print(bigname,bigval)
+
+'''
+#another cleaner way using .split() method
+
+fh = open("mbox-short.txt")
+mailid = dict()
+words = list()
+for line in fh:
+    words = line.split() #default delimiter is a space and treats many spaces consecutively as single space
+    if not "From" in words: continue #skip code
+    for i in range(len(words)): #range gives a list of indices while the len just gives a fixed value
+        if words[i] == "From":
+            key = words[i+1]
+            mailid[key] = mailid.get(key,0) + 1  #histogram idiom
+#max looping through the dictionary
+bigname = None
+bigval = None
+for mailname,mailcount in mailid.items():
+    if bigval is None or mailcount > bigval:
+        bigval = mailcount
+        bigname = mailname
+
+print(bigname,bigval)       
