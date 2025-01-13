15fa4327a7e05eb03b122cc61968b3a4d0e5e72d
effinrinesh <effinrineshj@gmail.com>
2025-01-13 11:38:58 +0000

    added urllib (opening http pages as a file handle)

---
+++ python-coding-practice/ex12-URLLIB.py
@@ -0,0 +1,6 @@
+#USING URLLIB IN PLACE OF SOCKETS
+
+import urllib.request, urllib.parse, urllib.error
+fhand = urllib.request.urlopen("http://data.pr4e.org/romeo.txt")
+for line in fhand:
+    print(line.decode().strip())
